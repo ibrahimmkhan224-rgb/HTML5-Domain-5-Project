8300f7e124e22aa911609a04b6ae4c146832869c
ibrahimmkhan224-rgb <ibrahimmkhan224@gmail.com>
2026-02-03 12:19:10 -0500

    commit 2/3/26

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("js console");
 let data;
-let grid = doucment.querySelector(".grid-container");
+let grid = document.querySelector(".movie-grid");
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
@@ -28,5 +28,60 @@ xhttp.onreadystatechange = function(){
         }
     };
 
-    xhttp.open("GET", "gamedata.json", true);
+    xhttp.open("GET", "showbox.json", true);
     xhttp.send();
+
+    /* 
+    console.log("js started");
+
+var data;
+var grid = document.querySelector(".grid-container");
+
+// LOAD DATA (localStorage first, otherwise XHR)
+if (localStorage.getItem("datalist")) {
+  data = JSON.parse(localStorage.getItem("datalist"));
+  console.log("Loaded from localStorage");
+  if (grid) {
+    makeCards();
+  }
+} else {
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText);
+      console.log("Loaded from gameData.json");
+
+      localStorage.setItem("datalist", JSON.stringify(data));
+      console.log("Saved starter data to localStorage");
+
+      if (grid) {
+        makeCards();
+      }
+    }
+  };
+
+  xhttp.open("GET", "gameData.json", true);
+  xhttp.send();
+}
+
+// RENDER CARDS
+function makeCards() {
+  grid.innerHTML = "";
+
+  data.forEach(function (game) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let textData =
+      "<div class='game-title'>" + game.title + "</div>" +
+      "<div>Publisher: " + game.publisher + "</div>" +
+      "<div>Release Date: " + game.releaseDate + "</div>";
+
+    card.innerHTML = textData;
+    grid.appendChild(card);
+  });
+
+  console.log("cards refreshed");
+}
+*/
